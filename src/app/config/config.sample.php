079ba42e711c6788e0b088d3aed1a57175f025a3
gillout <gillout@bbox.fr>
2020-11-06 16:48:34 +0100

    Fix : correction du config.sample.php

--- src/app/config/config.sample.php
+++ src/app/config/config.sample.php
@@ -3,8 +3,8 @@
 define ('ROOT_DIR', dirname(__DIR__) . DIRECTORY_SEPARATOR);
 
 const CONFIG = [
-    'db.pilote' => 'mysql',
-    'db.driver' => '127.0.0.1',
+    'db.driver' => 'mysql',
+    'db.host' => '127.0.0.1',
     'db.name' => 'bookstore',
     'db.user' => 'root',
     'db.password' => '',
