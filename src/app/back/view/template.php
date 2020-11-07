7f602d5fe768c33ae0d83196b34098387a1dfb08
gillout <gillout@bbox.fr>
2020-11-07 15:23:21 +0100

    Feat : Modification de nom du script

--- src/app/back/view/template.php
+++ src/app/back/view/template.php
@@ -84,7 +84,7 @@ if (isset($section)) {
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"
         integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV"
         crossorigin="anonymous"></script>
-<script src="script/form.js"></script>
+<script src="script/book-form.js"></script>
 </body>
 </html>
 <?php
